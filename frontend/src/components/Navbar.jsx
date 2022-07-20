77b8c092f44d3d6597dd4124b27d2e556461599c
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-20 10:27:06 +0200

    Basic navbar with responsive style

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,11 +4,13 @@ const makeLink = (path, text) => <Link to={path}>{text}</Link>;
 
 function Navbar() {
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <p>LOGO</p>
-        <ul>
+    <nav className="p-12">
+      <div className="nav-wrapper flex justify-center lg:justify-between">
+        <p className="hidden lg:block">LOGO</p>
+        <ul className="flex gap-8 text-xl">
           <li>{makeLink("/", "Home")}</li>
+          <li>{makeLink("#", "Projects")}</li>
+          <li>{makeLink("#", "Contact")}</li>
         </ul>
       </div>
     </nav>
