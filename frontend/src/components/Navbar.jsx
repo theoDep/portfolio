61954e017e44eddcb3e55593b4267a19f8524e7d
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-20 09:51:19 +0200

    Add basic structure to navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
+const makeLink = (path, text) => <Link to={path}>{text}</Link>;
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <p>LOGO</p>
+        <ul>
+          <li>{makeLink("/", "Home")}</li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
