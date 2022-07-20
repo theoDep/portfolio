6d6ca060e50ef0c669a5df3bfb9ebbe579d29133
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-20 09:51:40 +0200

    Add Router helper component to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Navbar from "@components/Navbar";
+import Router from "./helpers/Router";
 
 import App from "./App";
 
@@ -7,6 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <App />
+    </Router>
   </React.StrictMode>
 );
