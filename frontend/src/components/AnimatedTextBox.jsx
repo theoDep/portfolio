847e8f462c1c2ddb08a3f879a165d064b10a680d
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-20 11:27:19 +0200

    Create AnimatedTextBox with content & style

---
+++ frontend/src/components/AnimatedTextBox.jsx
@@ -0,0 +1,14 @@
+const spanStyleRight = "block text-4xl slide-right lg:text-8xl";
+const spanStyleLeft = "block text-4xl slide-left lg:text-8xl";
+
+function AnimatedTextBox() {
+  return (
+    <div className="text-container text-center lg:pl-12 lg:text-start lg:pt-8">
+      <span className={spanStyleRight}>Hey, I'm Theo</span>
+      <span className={spanStyleLeft}>A passionate</span>
+      <span className={spanStyleRight}>Fullstack Developer</span>
+    </div>
+  );
+}
+
+export default AnimatedTextBox;
