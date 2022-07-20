633123c6c5c17e1436286e7002b9fc28f3fb7fe1
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-20 11:31:50 +0200

    Change mt in Home.jsx and add media query

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import AnimatedTextBox from "@components/AnimatedTextBox";
 
 export default function Home() {
   return (
-    <main className="relative mt-20">
+    <main className="relative mt-8 lg:mt-20">
       <AnimatedTextBox />
     </main>
   );
