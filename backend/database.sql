3b7582c40e68a4c71787c153895710cbdb503055
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-20 18:59:00 +0200

    Add data in database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -1,63 +1,56 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `simple-mvc`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `item`
---
-
-CREATE TABLE `item` (
+CREATE TABLE `tech` (
   `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `item`
---
 
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
+INSERT INTO `tech` (`id`, `name`) VALUES
+(1, 'Javascript'),
+(2, 'PHP'),
+(3, 'NodeJS'),
+(4, 'ExpressJS'),
+(5, 'mySQL');
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
+ALTER TABLE `tech`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pour les tables exportées
---
+ALTER TABLE `tech`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `project` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NULL,
+  `image` varchar(255) NOT NULL,
+  `link` varchar(255) NULL,
+  `repo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `project` (`id`, `title`, `description`, `image`, `link`, `repo`) VALUES
+(1, 'Hero Corporation', "Réalisation dans le cadre de la Wild Code School d'un application web dynamique React consommant une API", 'https://i.imgur.com/XbAgQS7.png', 'https://theodep.github.io/Hero-Corporation/', 'https://github.com/theoDep/Hero-Corporation');
+
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `tech`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+CREATE TABLE `project_tech` (
+  `project_id` int(11) UNSIGNED NOT NULL,
+  CONSTRAINT fk_project_tech_project
+        FOREIGN KEY (project_id)
+        REFERENCES project(id),
+  `tech_id` int(11) UNSIGNED NOT NULL,
+  CONSTRAINT fk_project_tech_tech
+        FOREIGN KEY (tech_id)
+        REFERENCES tech(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `project_tech` (`project_id`,`tech_id`) VALUES
+(1, 1),
+(1, 3),
+(1, 4);
+  
