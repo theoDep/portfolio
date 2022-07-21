b6e989543414a225a710592969d579d9d5b57869
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-21 09:49:29 +0200

    Create project routes in router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { TechController } = require("./controllers");
+const { TechController, ProjectController } = require("./controllers");
 
 const router = express.Router();
 
@@ -9,5 +9,11 @@ router.get("/techs/:id", TechController.read);
 router.put("/techs/:id", TechController.edit);
 router.post("/techs", TechController.add);
 router.delete("/techs/:id", TechController.delete);
+
+router.get("/projects", ProjectController.browse);
+router.get("/projects/:id", ProjectController.read);
+router.put("/projects/:id", ProjectController.edit);
+router.post("/projects", ProjectController.add);
+router.delete("/projects/:id", ProjectController.delete);
 
 module.exports = router;
