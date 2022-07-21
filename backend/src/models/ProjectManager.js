d455f85c3271b5d585877442eb888bf91359f5ce
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-21 16:21:36 +0200

    Add findAll and findTechs methods to ProjectManager.js

--- backend/src/models/ProjectManager.js
+++ backend/src/models/ProjectManager.js
@@ -1,7 +1,21 @@
 const AbstractManager = require("./AbstractManager");
+const ProjectTechManager = require("./ProjectTechManager");
+const TechManager = require("./TechManager");
 
 class ProjectManager extends AbstractManager {
   static table = "project";
+
+  findAll() {
+    return this.connection.query(`
+    SELECT DISTINCT project.id, project.title, project.description, project.image, project.link, project.repo FROM ${ProjectManager.table} INNER JOIN ${ProjectTechManager.table} ON project.id = projectTech.project_id INNER JOIN ${TechManager.table} ON projectTech.tech_id = tech.id`);
+  }
+
+  findTechs(projectId) {
+    return this.connection.query(
+      `SELECT DISTINCT ${TechManager.table}.name from ${ProjectTechManager.table} INNER JOIN ${TechManager.table} ON tech.id = projectTech.tech_id where project_id = ?`,
+      [projectId]
+    );
+  }
 
   insert(project) {
     return this.connection.query(
